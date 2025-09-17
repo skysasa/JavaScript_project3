4e2a163f385f97e89134aa7ae896ba9ebc6cb733
Eunyoung Jang <bestsasa.sky@gmail.com>
2025-09-17 18:44:42 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const API_KEY = `8326ff18cc064d39a0460f49562eb7d3`;
+let newsList = [];
+const getLatesNews = async () => {
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+  //   console.log("uuu", url);
+  //fetch 호출 정보를 가져올려는 함수
+  //async(비동기 함수 처리) - await (데이터 받을때 까지 기다려주는것)
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log("ddd", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map((news) => {
+      let summary = "";
+      if (news.description) {
+        if (news.description.length > 200) {
+          summary = news.description.substring(0, 200) + "...";
+        } else {
+          summary = news.description;
+        }
+      } else {
+        summary = "내용없음.";
+      }
+
+      const defaultImage = ".img/image_not_available.png";
+      let imageURL;
+      if (news.urlToImage) {
+        imageURL = news.urlToImage;
+      } else {
+        imageURL = defaultImage;
+      }
+
+      // let checkSource=""
+      // if(news.source.name) {
+      //   checkSource = news.source.name
+      // } else {
+      //   checkSource="no source"
+      // } // 간단한 버전은
+      let checkSource = news.source.name || "no source";
+
+      let timeFormat = moment(news.publishedAt).format(
+        "MMMM Do YYYY, h:mm:ss a"
+      );
+
+      return `<div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src="${imageURL}"
+            />
+          </div>
+          <div class="col-lg-8 news-info">
+            <h2>${news.title}</h2>
+            <p>${summary}</p>
+            <div>${checkSource} | ${timeFormat}</div>
+          </div>
+        </div>`;
+    })
+    .join("");
+  document.getElementById(`news-board`).innerHTML = newsHTML;
+};
+getLatesNews();
