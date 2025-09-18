b57ae2d518897aad3d7e62ff44996255c5cbef96
Eunyoung Jang <bestsasa.sky@gmail.com>
2025-09-17 21:57:49 -0400

    css fixed

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
 const API_KEY = `비밀`;
 let newsList = [];
 const getLatesNews = async () => {
